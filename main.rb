043995fae283dd43c4a898de273203bb2b1a71ba
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-20 14:19:49 +0900

    fix initialize method

--- main.rb
+++ main.rb
@@ -2,11 +2,15 @@ class Brave
 
   # initializeメソッド
   # インスタンス作成時に初期値を設定する。
-  def initialize
-    @name = "テリー"
-    @hp = 500
-    @offense = 150
-    @defense = 100
+
+  # paramsで一括で受け取る
+  # 引数に**を記述：ハッシュしか受け取れなくなる
+  def initialize(**params)
+    # 各パラメータをハッシュで取得
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
   end
   
   # name getter
@@ -31,7 +35,8 @@ class Brave
   
 end
 
-brave = Brave.new
+# キーワード変数
+brave = Brave.new(name: "テリー",hp: 500,offense: 150,defense: 100)
 
 puts <<~TEXT
 NAME: #{brave.getName}
