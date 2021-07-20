db349ced2e5740662296c34458072a416e07d88d
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-20 14:30:15 +0900

    refactoring

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 class Brave
+  # attr_readerの記述でゲッターを省略することができる
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
 
   # initializeメソッド
   # インスタンス作成時に初期値を設定する。
-
   # paramsで一括で受け取る
   # 引数に**を記述：ハッシュしか受け取れなくなる
   def initialize(**params)
@@ -13,34 +15,18 @@ class Brave
     @defense = params[:defense]
   end
   
-  # name getter
-  def getName
-    @name
-  end
-
-  # hp getter
-  def getHp
-    @hp
-  end
-  
-  # offense getter
-  def getOffense
-    @offense
-  end
-  
-  # defense getter
-  def getDefense
-    @defense
-  end
-  
 end
 
-# キーワード変数
+# ハッシュとして送信
 brave = Brave.new(name: "テリー",hp: 500,offense: 150,defense: 100)
 
 puts <<~TEXT
-NAME: #{brave.getName}
-HP: #{brave.getHp}
-OFFENSE: #{brave.getOffense}
-DEFENSE: #{brave.getDefense}
+NAME: #{brave.name}
+HP: #{brave.hp}
+OFFENSE: #{brave.offense}
+DEFENSE: #{brave.defense}
 TEXT
+
+brave.hp -= 30
+
+puts "#{brave.name}はダメージを受けた。残りのHPは#{brave.hp}だ"
