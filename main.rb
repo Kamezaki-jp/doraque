6b6b2a1d0fefcbe9a551f214f93f1d5a67944ce1
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-20 16:18:47 +0900

    refactoring Brave Class

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ class Brave
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   # initializeメソッド
   # インスタンス作成時に初期値を設定する。
   # paramsで一括で受け取る
@@ -18,26 +20,61 @@ class Brave
   def attack(monster)
     puts "#{@name}の攻撃！"
 
-    attack_num = rand(4)
-    if attack_num == 0
-      puts "クリティカルヒット！"
-      damage = calculate_special_attack - monster.defense
-    else
-      puts "通常攻撃"
-      damage = @offense - monster.defense
-    end
+    # decision_attack_typeメソッドの呼び出し
+    attack_type = decision_attack_type
 
-    monster.hp -= damage
-
-    puts "#{monster.name}は#{damage.floor}のダメージを受けた！"
+    # calculate_damageメソッドの呼び出し
+    damage = calculate_damage(target: monster,attack_type: attack_type)
+    
+    # ダメージをHPに反映させる
+    cause_damage(target: monster,damage: damage)
+    
     puts "#{monster.name}の残りHPは、#{monster.hp.floor}だ！"
-
-  end
-
-  def calculate_special_attack
-    @offense * 1.5
   end
   
+
+  private
+    # 通常攻撃かクリティカルヒットか判定をするメソッド
+    def decision_attack_type
+      attack_num = rand(4)
+      
+      if attack_num == 0
+        puts "クリティカルヒット！"
+        "special_attack"
+      else
+        puts "通常攻撃"
+        "normal_attack"
+      end
+    end
+    
+    # ダメージ計算のメソッド
+    def calculate_damage(**params)
+      # 変数に格納することによって将来ハッシュのキーに変更があった場合でも変更箇所が少なくて済む
+      target = params[:target]
+      attack_type = params[:attack_type]
+
+      if attack_type == "special_attack"
+        calculate_special_attack - target.defense
+      else
+        @offense - target.defense
+      end
+    end
+
+    # ダメージ反映させる
+    def cause_damage(**params)
+
+      target = params[:target]
+      damage = params[:damage]
+
+      target.hp -= damage
+
+      puts "#{target.name}は#{damage}のダメージを受けた！"
+    end
+    
+    def calculate_special_attack
+      @offense * SPECIAL_ATTACK_CONSTANT
+    end
+    
 end
 
 class Monster
