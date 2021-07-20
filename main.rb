1486c1a763a4a2a07d10a113c2152e520c2e38ee
Kamezaki-jp <champagne.es335@gmail.com>
2021-07-20 14:06:03 +0900

    add Brave Class

--- main.rb
+++ main.rb
@@ -1,1 +1,55 @@
-#test
+class Brave
+  #nameのセッター
+  def name=(name)
+    @name = name
+  end
+
+  # name getter
+  def getName
+    @name
+  end
+
+  # HPのセッター
+  def hp=(hp)
+    @hp = hp
+  end
+
+  # hp getter
+  def getHp
+    @hp
+  end
+
+  # Offenseのセッター
+  def offense=(offense)
+    @offense = offense
+  end
+
+  # offense getter
+  def getOffense
+    @offense
+  end
+
+  # defenseのセッター
+  def defense=(defense)
+    @defense = defense
+  end
+
+  # defense getter
+  def getDefense
+    @defense
+  end
+  
+end
+
+brave = Brave.new
+brave.name = "テリー"
+brave.hp = 500
+brave.offense = 150
+brave.defense = 100
+
+puts <<~TEXT
+NAME: #{brave.getName}
+HP: #{brave.getHp}
+OFFENSE: #{brave.getOffense}
+DEFENSE: #{brave.getDefense}
+TEXT
